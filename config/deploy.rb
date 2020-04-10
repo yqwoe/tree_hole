ea7af0d0122c2b7c3f64a4c61092f87944c304e1
yqwoe <yuangzhangchn@gmail.com>
2020-04-10 14:04:43 +0800

    add nginx conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,20 +3,29 @@ lock "~> 3.13.0"
 
 set :application, "tree_hole"
 set :repo_url, "git@github.com:yqwoe/tree_hole.git"
+set :branch, 'master'
 
+set :rails_env, 'production'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :user, 'root'
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/tree_hole"
-
+set :deploy_to,       "/home/#{fetch(:user)}/production/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false  # Change to tr
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
 # Default value for :pty is false
 # set :pty, true
 
