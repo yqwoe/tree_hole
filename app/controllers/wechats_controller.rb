8e6b44994df0f04c285506bfbc941dd890930bb7
yqwoe <yuangzhangchn@gmail.com>
2020-04-10 16:21:41 +0800

    add nginx conf

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -3,6 +3,19 @@ class WechatsController < ApplicationController
   wechat_responder
 
   on :text do |request, content|
+    puts request
     request.reply.text "echo: #{JSON.stringfy(request)} #{content}" # Just echo
   end
+
+
+  # 当用户加关注
+  on :event, with: 'subscribe' do |request|
+    request.reply.text "亲爱的,#{request[:FromUserName]} ! 欢迎回家"
+  end
+
+  # 当用户取消关注订阅
+  on :event, with: 'unsubscribe' do |request|
+    request.reply.success # user can not receive this message
+  end
+
 end
